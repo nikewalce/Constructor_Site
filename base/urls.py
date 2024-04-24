cac4a9bf8927ec240af4ce2b80d36ca1b06fa712
nikewalce <nikewalce1@gmail.com>
2024-04-24 21:00:25 +0300

    Dynamic
    Добавлены динамические страницы

--- base/urls.py
+++ base/urls.py
@@ -6,8 +6,10 @@ from . import views
 #Тут отслеживают адресса
 #При переходе на base/ запускаем метод views. Круглые скобки не нужны(если ставить их, то метод выполняется. А тут нужно обратиться только)
 #'' срабатывает, когда переходят по base/
+#<int:pk> - отслеживание динамической записи с типом int
 urlpatterns = [
     path('', views.index, name='base'),
     path('data', views.db, name='db'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.BaseDetailView.as_view(), name='base_detail'),
 ]
--- base/views.py
+++ base/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from .models import Constructor
 #Импортируем обработчик формы
 from .forms import ConstructorForm
+#На основе этого метода можно создавать динамические страницы
+from django.views.generic import DetailView
 
 #отвечает за те методы, которые будут вызваны при переходе пользователя на определенную страницу
 #обязательно прописывать аргумент запроса(request)
@@ -11,15 +13,23 @@ def index(request):
     #render(request, 'директория с название приложения в templates HTML документ', словарь для передачи данных в шаблоны)
 
     #Получаю все объекты из таблицы Constructor, передаем в render
-    all_object = Constructor.objects.all()
+    #all_object = Constructor.objects.all()
 
     # #сортировка по title(в обратном порядке -title). можно выбрать кол-во записей: ('title')[:1]
-    # all_object = Constructor.objects.order_by('title')
+    all_object = Constructor.objects.order_by('-date')
 
     return render(request, 'base/base_index.html', {'all_object': all_object})
 
 def db(request):
     return HttpResponse('<h1>Проверка base/data </h1>')
+
+#Можно использовать ListView, тогда будет вызываться вся таблица
+class BaseDetailView(DetailView):
+    model = Constructor
+    template_name = 'base/detail_view.html'
+    #Имя ключа по которому можно передавать определенную запись внутрь шаблона
+    context_object_name = 'model_chair'
+
 
 def create(request):
     error=''
